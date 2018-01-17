e590a9b9582bd8d9df0de885699c48323582875e
Eyal Benmoshe <ebm2626@yahoo.com>
2018-01-17 15:37:20 -0800

    first commit

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+	render() {
+		return (
+			<form className="store-selector">
+				
+			</form>
+			)
+	}
+}
+
+export default StorePicker;
