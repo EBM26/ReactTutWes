608e82302542811ab1c7d69e7cb770a734c5124d
Eyal Benmoshe <ebm2626@yahoo.com>
2018-01-18 09:00:22 -0800

    did up to video 7

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -5,7 +5,10 @@ class StorePicker extends React.Component {
 	render() {
 		return (
 			<form className="store-selector">
-				
+				{/* this is how you comment in JSX */}
+				<h2>Please Enter a Store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store</button>
 			</form>
 			)
 	}
