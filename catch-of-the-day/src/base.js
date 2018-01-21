e7e57dc5374bc54ed7727ae95ae7b17fdc86ef6d
Eyal Benmoshe <ebm2626@yahoo.com>
2018-01-20 22:16:01 -0800

    did up to video 19

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	   apiKey: "AIzaSyB9GfXYtWAmWpfB2w4nOfg3Ao02YigER4A",
+       authDomain: "catch-of-the-day-eyal.firebaseapp.com",
+       databaseURL: "https://catch-of-the-day-eyal.firebaseio.com",
+});
+
+export default base ;
